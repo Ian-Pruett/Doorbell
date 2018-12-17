32a79e8491a3f6753bab677de9b96a14d6ec93a7
ipruett <ian.pruett@wsu.edu>
2018-12-16 20:39:27 -0800

    .

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 from machine import Pin
+import time
 
+def btn_led_event(p):
+    print('value of button is ', p)
+    D1.on()
+    time.sleep(1)
+    D1.off()
+
+D1 = Pin(5, Pin.OUT)
+D2 = Pin(4, Pin.IN)
+
+D2.irq(trigger=Pin.IRQ_FALLING, handler=btn_led_event)
